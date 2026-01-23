4df5ba20efe5044856880922b4f5b6d7ec8438e6
rastegaev <i@ivlad.ru>
2026-01-23 14:32:58 +0500

    fix

--- migrations/20260123092340-add-email-login-fields.js
+++ migrations/20260123092340-add-email-login-fields.js
@@ -3,19 +3,19 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.addColumn('Users', 'emailLoginCode', {
+    await queryInterface.addColumn('users', 'emailLoginCode', {
       type: Sequelize.STRING,
       allowNull: true
     });
 
-    await queryInterface.addColumn('Users', 'emailLoginCodeExpiry', {
+    await queryInterface.addColumn('users', 'emailLoginCodeExpiry', {
       type: Sequelize.DATE,
       allowNull: true
     });
   },
 
   async down (queryInterface, Sequelize) {
-    await queryInterface.removeColumn('Users', 'emailLoginCode');
-    await queryInterface.removeColumn('Users', 'emailLoginCodeExpiry');
+    await queryInterface.removeColumn('users', 'emailLoginCode');
+    await queryInterface.removeColumn('users', 'emailLoginCodeExpiry');
   }
 };
