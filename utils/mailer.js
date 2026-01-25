9e655ff03ec5d86499774e73203263a5103bd968
rastegaev <i@ivlad.ru>
2026-01-26 00:52:52 +0500

    fix

--- utils/mailer.js
+++ utils/mailer.js
@@ -5,8 +5,8 @@ const transporter = nodemailer.createTransport({
   port: 587,
   secure: false,
   auth: {
-    user: 'info@mailer.carismamp.com',
-    pass: '242CaRismA516716!@#',
+    user: process.env.SMTP_LOGIN,
+    pass: process.env.SMTP_PASS,
   },
 });
 
